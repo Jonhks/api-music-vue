d54efef84b4b601013a942e6e9966364e92ba969
jonhks <jonathan@laboratoria.la>
2018-08-24 10:43:10 -0500

    pintando artistas con fetch, primer componente

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+// Nombre de la aplicación	first-proyect-vue
+// Clave API	1a438a498c6d7e642346cb1cbd464787
+// Secreto compartido	f5454bc84cf60e514c3aeb8990d52ef8
+// Registrado a	Jonhks
+
+export default {
+  appName: 'first-proyect-vue',
+  apiKey: '1a438a498c6d7e642346cb1cbd464787',
+  secret: 'f5454bc84cf60e514c3aeb8990d52ef8',
+  registeredTo: 'Jonhks'
+}
---
+++ src/components/artist.vue
@@ -0,0 +1,23 @@
+<template lang = "pug">
+  li
+    img(class="img" :src="artist.image[4]['#text']")
+    h2: a(:href="artist.url" target="_blank") {{ artist.name }}
+</template>
+
+<script>
+export default {
+  name: 'artist',
+  props: ['artist']
+}
+</script>
+
+<style lang="stylus">
+  li
+    display block
+    margin 10 0px
+
+  .img 
+    width 50%
+    border 3px black outset
+
+</style>
